cc9feb5f159bd23fb34a726ff916ec792b15433e
q3dm42 <sergoteq@gmail.com>
2025-05-21 10:06:50 +0300

    исправил оставшееся вложения на async/await

--- modules/api.js
+++ modules/api.js
@@ -1,39 +1,32 @@
 const apiUrl = "https://wedev-api.sky.pro/api/v1/sergei-smirnov/comments";
 
 //запостить коммент
-export function postComment(name, text) {
-  return fetch(apiUrl, {
+export async function postComment(name, text) {
+  const response = await fetch(apiUrl, {
     method: "POST",
     headers: {},
     body: JSON.stringify({
       name: name,
       text: text,
     }),
-  }).then((response) => {
-    if (response.status === 400) {
-      return response
-        .json()
-        .then((data) =>
-          Promise.reject(
-            new Error(data.message || "Вы ввели имя короче 3-х символов")
-          )
-        );
-    }
-    if (response.status === 500) {
-      return Promise.reject(new Error("Ошибка сервера"));
-    }
-    if (response.status === 201) {
-      return response.json();
-    }
   });
+  if (response.status === 400) {
+    const data = await response.json();
+    throw new Error(data.message || "Вы ввели имя короче 3-х символов");
+  }
+  if (response.status === 500) {
+    throw new Error("Ошибка сервера");
+  }
+  if (response.status === 201) {
+    return response.json();
+  }
 }
 
 //получить комменты с сервера
-export function fetchComments() {
-  return fetch(apiUrl).then((response) => {
-    if (response.status === 500) {
-      return Promise.reject(new Error("Сервер поломался"));
-    }
-    return response.json();
-  });
+export async function fetchComments() {
+  const response = await fetch(apiUrl);
+  if (response.status === 500) {
+    throw new Error("Сервер поломался");
+  }
+  return response.json();
 }
